6be810b9675a6643bb7f2c72c48c7d3bcb5d456c
answj <moonshot@ajou.ac.kr>
2026-01-02 00:18:32 +0900

    stirng class, 사원관리 프로그램

--- main.cpp
+++ main.cpp
@@ -1,16 +1,131 @@
-/*ìƒìˆ˜ í¬ì¸í„°?*/
-#include<iostream>
+#include <iostream>
+#include <string>
 
-void swap(int*a, int *b){
-    int temp =*a;
-    *a=*b;
-    *b=temp;
+class Manager{
+    std::string name;
+    int age;
+
+    std::string position;
+    int rank;
+    int year_of_service;
+
+    public :
+        Manager(std::string name, int age, std::string position, int rank, int year_of_service)
+        :year_of_service(year_of_service),
+         name(name),
+         age(age),
+         position(position),
+         rank(rank) {}
+
+    Manager(const Manager& manager){
+        name = manager.name;
+        age = manager.age;
+        position = manager.position;
+        rank = manager.rank;
+        year_of_service = manager.year_of_service;
+    }
+
+    Manager() {}
+    int calculate_pay() { return 200 + rank * 50 + 5 * year_of_service;}
+    void print_info(){
+        std::cout <<name << " ( "<< position << " , " << age << ", "
+        << year_of_service<< "³âÂ÷) ==>" <<calculate_pay() <<"¸¸¿ø"<<std::endl;
+    }
+};
+
+class Employee{
+    std::string name;
+    int age;
+
+    std::string position;
+    int rank;
+
+    public:
+        Employee(std::string name, int age, std::string position, int rank)
+            : name(name), age(age), position(position), rank(rank) {}
+
+Employee(const Employee& employee){
+    name = employee.name;
+    age = employee.age;
+    position = employee.position;
+    rank = employee.rank;
 }
 
-int main(){
-    int x = 10, y=20;
-    swap(&x, &y);
+Employee() {}
 
-    std::cout << x <<" " <<y<< std::endl;
+void print_info(){
+    std::cout <<name << "(" << position << " , " << age << " ) ==>" 
+    <<calculate_pay() <<  " ¸¸¿ø " << std::endl;
+
+}
+int calculate_pay() {return 200+ rank * 50;}
+};
+
+class EmployeeList{
+    int alloc_employee; 
+    int current_employee; 
+    int current_manager;
+
+    Employee **employee_list; 
+    Manager **manager_list;
+
+    public: 
+        EmployeeList(int alloc_employee) : alloc_employee(alloc_employee){
+            employee_list = new Employee*[alloc_employee];
+            manager_list = new Manager*[alloc_employee];
+            current_employee = 0;
+            current_manager = 0;
+        }
+
+    void add_employee(Employee* employee){
+        employee_list[current_employee] = employee;
+        current_employee++;
+    }
+
+    void add_manager(Manager* manager){
+        manager_list[current_manager] = manager;
+        current_manager++;
+    }
+    int current_employee_num() {return current_employee + current_manager;}
+
+    void print_employee_info(){
+        int total_pay = 0;
+        for (int i =0; i <current_employee; i++){
+            employee_list[i]->print_info();
+            total_pay += employee_list[i]-> calculate_pay();
+        }
+         for (int i =0; i <current_manager; i++){
+            manager_list[i]->print_info();
+            total_pay += manager_list[i]-> calculate_pay();
+        }
+
+        std::cout <<" ÃÑ ºñ¿ë : " << total_pay << "¸¸¿ø"<< std::endl;
+
+    }
+    ~EmployeeList(){
+        for(int i =0; i <current_employee; i++){
+            delete employee_list[i];
+        }
+         for(int i =0; i <current_manager; i++){
+            delete manager_list[i];
+        }
+        delete[] employee_list;
+          delete[] manager_list;
+    }
+};
+int main(){
+    EmployeeList emp_list(10);
+
+
+   emp_list.add_employee(new Employee("³ëÈ«Ã¶", 34, "Æò»ç¿ø", 1));
+   emp_list.add_employee(new Employee("ÇÏÇÏ", 34, "Æò»ç¿ø", 1));
+
+   emp_list.add_manager(new Manager("À¯Àç¼®", 41, "ºÎÀå", 7,12));
+   emp_list.add_manager(new Manager("Á¤ÁØÇÏ", 43, "°úÀå", 4,15));
+   emp_list.add_manager(new Manager("¹Ú¸í¼ö", 43, "Â÷Àå", 5,13));
+   emp_list.add_employee(new Employee("Á¤Çüµ·", 36, "´ë¸®", 2));
+   emp_list.add_employee(new Employee("±æ", 36, "ÀÎÅÏ", -2));
+
+    emp_list.print_employee_info();
     return 0;
 }

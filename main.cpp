55fc51a7fc3576e3f882db8fadb7128f1319b813
answj <moonshot@ajou.ac.kr>
2026-01-17 00:22:48 +0900

    연산자 연산 디테일

--- main.cpp
+++ main.cpp
@@ -13,6 +13,15 @@ class Complex{
         Complex operator-(const Complex& c);
         Complex operator*(const Complex& c);
         Complex operator/(const Complex& c);
+        
+
+        
+        Complex& operator+=(const Complex& c);
+        Complex& operator-=(const Complex& c);
+        Complex& operator*=(const Complex& c);
+        Complex& operator/=(const Complex& c);
+        
+        Complex& operator=(const Complex& c);
 
         void println() {std::cout << "(" << real << " , " << img << " ) "<< std::endl;}
 
@@ -38,12 +47,33 @@ Complex Complex::operator/(const Complex& c){
     return temp;
 }
 
+Complex& Complex::operator=(const Complex& c){
+    real = c.real;
+    img = c.img;
+    return *this;
+}
+
+   Complex& Complex::operator+=(const Complex& c){
+    (*this)=(*this) +c;
+    return *this;}
+           Complex& Complex::operator-=(const Complex& c){
+    (*this)=(*this) -c;
+    return *this;}
+           Complex& Complex::operator*=(const Complex& c){
+    (*this)=(*this) *c;
+    return *this;}
+           Complex& Complex::operator/=(const Complex& c){
+    (*this)=(*this)/c;
+    return *this;}
+
 
 int main(){
     Complex a(1.0,2.0);
     Complex b(3.0, -2.0);
-
-    Complex c = a-b;
-    c.println();
+    Complex c(0.0, 0.0);
+    a+=b;
+    a.println();
+    b.println();
+    
 }
 

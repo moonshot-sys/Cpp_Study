d7df2f36749ba9a2eebe834ab8c6e895eb725abf
answj <moonshot@ajou.ac.kr>
2026-01-17 00:12:47 +0900

    연산자를 활용한 복소수 계산 코드

--- main.cpp
+++ main.cpp
@@ -1,126 +1,49 @@
-#include <string.h>
+
 #include <iostream>
 
-class MyString{
-    char* string_content; //문자열 데이터를 가리키는 포인터
-    int string_length;  // 문자열 길이
-
-    int memory_capacity; //현재 할당된 용량
+class Complex{
+    private:
+        double real, img;
 
     public:
-    MyString(char c);
-    MyString(const char* str);
-    MyString(const MyString& str);
-    ~MyString();
+        Complex(double real, double img) : real(real), img(img){}
+        Complex(const Complex& c) {real = c.real, img = c.img; }
 
-    int lenght() const;
-    int capacity() const;
+        Complex operator+(const Complex& c);
+        Complex operator-(const Complex& c);
+        Complex operator*(const Complex& c);
+        Complex operator/(const Complex& c);
 
-    void reserve(int size);
-
-    void print() const;
-    void println() const;
-
-    char at(int i) const;
-
-
-    int compare(MyString& str);
-    bool operator==(MyString& str);
+        void println() {std::cout << "(" << real << " , " << img << " ) "<< std::endl;}
 
 };
 
-MyString::MyString(char c){
-    string_content = new char[1];
-    string_content[0] = c;
-    memory_capacity = 1;
-    string_length = 1;
-}
-MyString::MyString(const char* str){
-    string_length = strlen(str);
-    memory_capacity = string_length;
-    string_content = new char[string_length];
-
-    for (int i =0; i != string_length; i++) string_content[i]=str[i];
-}
-MyString::MyString(const MyString& str){
-string_length = str.string_length;
-string_content = new char[string_length];
-
-for (int i = 0; i != string_length; i++)
-    string_content[i] = str.string_content[i];
-}
-MyString::~MyString() {delete[] string_content;}
-int MyString::lenght() const{return string_length;}
-void MyString::print() const{
-    for (int i =0; i != string_length; i++) std::cout << string_content[i];
-
-}
-void MyString::println() const{
-    for (int i = 0; i != string_length; i++) std::cout << string_content[i];
-
-    std::cout << std::endl;
-
+Complex Complex::operator+(const Complex& c){
+    Complex temp(real + c.real, img + c.img);
+    return temp;
 }
 
-int MyString::capacity() const {return memory_capacity;}
-void MyString::reserve(int size){
-    if(size > memory_capacity){
-    char* prev_string_content = string_content;
-
-    string_content = new char[size];
-    memory_capacity = size;
-
-    for (int i = 0; i != string_length; i++)
-        string_content[i] = prev_string_content[i];
-
-    delete[] prev_string_content;
-}
+Complex Complex::operator-(const Complex& c){
+    Complex temp(real - c.real, img - c.img);
+    return temp;
 }
 
-char MyString::at(int i) const{
-    if (i>=string_length || i<0)
-        return 0;
-    else
-        return string_content[i];
+Complex Complex::operator*(const Complex& c){
+    Complex temp(real * c.real - img * c.img, real * c.img + img *c.real);
+    return temp;
 }
 
-int MyString::compare(MyString& str){
-
-
-    for (int i = 0; i <std::min(string_length, str.string_length); i++){
-        if (string_content[i] > str.string_content[i])
-            return 1;
-
-        else if (string_content[i] < str.string_content[i])
-            return -1;
-
-    }
-
-    if (string_length == str.string_length) return 0;
-
-    else if (string_length > str.string_length)
-        return 1;
-
-    return -1;
+Complex Complex::operator/(const Complex& c){
+    Complex temp((real * c.real + img * c.img)/(c.real * c.real + c.img * c.img), (img*c.real - real * c.img) / (c.real * c.real + c.img*c.img));
+    return temp;
 }
 
-bool MyString::operator==(MyString& str){
-    return !compare(str);
-}
 
 int main(){
-    MyString str1("a word");
-    MyString str2("sentence");
-    MyString str3("sentence");
+    Complex a(1.0,2.0);
+    Complex b(3.0, -2.0);
 
-    if(str1 == str2)
-        std::cout << "str1와 str2 같다."<< std::endl;
-    else 
-        std::cout << "str1와 str2는 다르다."<< std::endl;
-
-    if(str2 == str3)
-        std::cout << "str2 와 str3 는 같다." << std::endl;
-    else
-        std::cout << "str2와 str3는 다르다" << std::endl;
+    Complex c = a-b;
+    c.println();
 }
 

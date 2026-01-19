8940eaab507946de932141b848d89962da88fb15
answj <moonshot@ajou.ac.kr>
2026-01-19 22:55:13 +0900

    잡다한 연산자 오버로딩

--- main.cpp
+++ main.cpp
@@ -1,20 +1,29 @@
+ //이 파일은 연습용이다.
+// 틀려도 되고, 정리 안 해도 된다.
 
 #include <iostream>
+#include <cstring>
 
 class Complex{
     private:
         double real, img;
 
+        double get_number(const char* str, int from, int to);
+
     public:
         Complex(double real, double img) : real(real), img(img){}
         Complex(const Complex& c) {real = c.real, img = c.img; }
+        Complex(const char* str);
 
         Complex operator+(const Complex& c);
         Complex operator-(const Complex& c);
         Complex operator*(const Complex& c);
         Complex operator/(const Complex& c);
         
-
+Complex Complex::operator+(const char* str);
+Complex Complex::operator-(const char* str);
+Complex Complex::operator*(const char* str);
+Complex Complex::operator/(const char* str);
         
         Complex& operator+=(const Complex& c);
         Complex& operator-=(const Complex& c);
@@ -26,12 +35,67 @@ class Complex{
         void println() {std::cout << "(" << real << " , " << img << " ) "<< std::endl;}
 
 };
+Complex operator+(const Complex& a, const Complex& b){
+    Complex temp(a);
+    return temp.operator+(b);
+}
+Complex::Complex(const char* str){
+  int begin = 0, end = strlen(str);
+  img = 0.0;
+  real = 0.0;
 
-Complex Complex::operator+(const Complex& c){
-    Complex temp(real + c.real, img + c.img);
-    return temp;
+  int pos_i = -1;
+  for (int i = 0; i !=end; i++){
+    if(str[i] =='i'){
+        pos_i = i;
+        break;
+    }
+  }
+
+  if(pos_i == -1){
+    real = get_number(str, begin, end -1);
+    return;
+  }
+
+  real = get_number(str, begin, pos_i - 1);
+  img = get_number(str, pos_i + 1, end -1);
+
+  if (pos_i >= 1 && str[pos_i -1] == '-') img *= -1.0;
 }
 
+double Complex::get_number(const char* str, int from, int to){
+    bool minus = false;
+    if(from > to) return 0;
+    if(str[from] == '-') minus = true;
+    if(str[from] == '-' || str[from] == '+') from++;
+
+
+double num =0.0;
+double decimal = 1.0;
+
+bool integer_part = true;
+
+for (int i = from; i <=to; i ++){
+    if (isdigit(str[i]) && integer_part){
+        num *=10.0;
+        num += (str[i] - '0');
+    } else if(str[i] == '.')
+        integer_part = false;
+    else if (isdigit(str[i]) && !integer_part){
+        decimal /=10.0;
+        num +=((str[i] - '0') * decimal);
+    
+    }else
+        break;
+}
+if(minus) num *=-1.0;
+return num;
+
+}
+Complex Complex::operator+(const Complex& c){
+   Complex temp(real + c.real, img + c.img);
+    return temp;
+}
 Complex Complex::operator-(const Complex& c){
     Complex temp(real - c.real, img - c.img);
     return temp;
@@ -53,6 +117,26 @@ Complex& Complex::operator=(const Complex& c){
     return *this;
 }
 
+Complex Complex::operator+(const char* str){
+    Complex temp(str);
+    return (*this) + temp;
+}
+
+Complex Complex::operator-(const char* str){
+    Complex temp(str);
+    return (*this) - temp;
+}
+Complex Complex::operator*(const char* str){
+    Complex temp(str);
+    return (*this) * temp;
+}
+
+Complex Complex::operator/(const char* str){
+    Complex temp(str);
+    return (*this) / temp;
+}
+
+
    Complex& Complex::operator+=(const Complex& c){
     (*this)=(*this) +c;
     return *this;}
@@ -66,14 +150,9 @@ Complex& Complex::operator=(const Complex& c){
     (*this)=(*this)/c;
     return *this;}
 
-
 int main(){
-    Complex a(1.0,2.0);
-    Complex b(3.0, -2.0);
-    Complex c(0.0, 0.0);
-    a+=b;
-    a.println();
-    b.println();
-    
+Complex a(0,0);
+a = "-1.1 + i3.923" + a;
+a.println();
 }
 

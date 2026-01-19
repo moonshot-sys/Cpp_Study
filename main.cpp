39b775397af42e9fee4e7eea587353167ece5b59
answj <moonshot@ajou.ac.kr>
2026-01-19 23:36:09 +0900

    첨가 연산자

--- main.cpp
+++ main.cpp
@@ -4,162 +4,71 @@
 #include <iostream>
 #include <cstring>
 
-class Complex{
-    private:
-        double real, img;
-
-        double get_number(const char* str, int from, int to);
+class MyString{
+    char* string_content; //문자열 데이터를 가리키는 포인터
+    int string_length; //문자열 길이
+    int memory_capacity; // 현재 할당량된 용량
 
     public:
-        Complex(double real, double img) : real(real), img(img){}
-        Complex(const Complex& c) {real = c.real, img = c.img; }
-        Complex(const char* str);
+    //문자 하나로 생성 
+    MyString(char c);
 
-        Complex operator+(const Complex& c);
-        Complex operator-(const Complex& c);
-        Complex operator*(const Complex& c);
-        Complex operator/(const Complex& c);
-        
-Complex Complex::operator+(const char* str);
-Complex Complex::operator-(const char* str);
-Complex Complex::operator*(const char* str);
-Complex Complex::operator/(const char* str);
-        
-        Complex& operator+=(const Complex& c);
-        Complex& operator-=(const Complex& c);
-        Complex& operator*=(const Complex& c);
-        Complex& operator/=(const Complex& c);
-        
-        Complex& operator=(const Complex& c);
+    //문자열로 부터 생성
+    MyString(const char* str);
 
-        friend Complex operator+(const Complex& a, const Complex& b);
-        friend std::ostream& operator<<(std::ostream& os, const Complex& c);
-        void println() {std::cout << "(" << real << " , " << img << " ) "<< std::endl;}
+    //복사 생성자
+    MyString(const MyString& str);
 
+    ~MyString();
+
+    int length();
+
+    void print() const;
+    void println() const;
+
+    char& operator[](const int index);
 };
-std::ostream& operator<<(std::ostream& os, const Complex& c){
-    os<<"(" << c.real << " , " << c.img << ")";
-    return os;
+
+MyString::MyString(char c){
+    string_content = new char[1];
+    string_content[0] = c;
+    memory_capacity = 1;
+    string_length =1;
+}
+MyString::MyString(const char* str){
+    string_length = strlen(str);
+    memory_capacity = string_length;
+    string_content = new char[string_length];
+
+    for(int i = 0; i != string_length; i ++) string_content[i] = str[i];
+}
+MyString::MyString(const MyString& str){
+    string_length = str.string_length;
+    string_content - new char[string_length];
+
+    for (int i =0; i != string_length; i++)
+        string_content[i] = str.string_content[i];
 
 }
-Complex operator+(const Complex& a, const Complex& b){
-    Complex temp(a);
-    return temp.operator+(b);
-}
-Complex::Complex(const char* str){
-  int begin = 0, end = strlen(str);
-  img = 0.0;
-  real = 0.0;
-
-  int pos_i = -1;
-  for (int i = 0; i !=end; i++){
-    if(str[i] =='i'){
-        pos_i = i;
-        break;
+MyString::~MyString() {delete[] string_content;}
+int MyString::length() {return string_length;}
+void MyString::print() const{
+    for(int i =0; i != string_length; i++){
+        std::cout<<string_content[i];
     }
-  }
-
-  if(pos_i == -1){
-    real = get_number(str, begin, end -1);
-    return;
-  }
-
-  real = get_number(str, begin, pos_i - 1);
-  img = get_number(str, pos_i + 1, end -1);
-
-  if (pos_i >= 1 && str[pos_i -1] == '-') img *= -1.0;
+}
+void MyString::println() const{
+    for(int i = 0; i != string_length; i++){
+        std::cout << string_content[i];
+    }
+    std::cout << std::endl;
 }
 
-double Complex::get_number(const char* str, int from, int to){
-    bool minus = false;
-    if(from > to) return 0;
-    if(str[from] == '-') minus = true;
-    if(str[from] == '-' || str[from] == '+') from++;
-
-
-double num =0.0;
-double decimal = 1.0;
-
-bool integer_part = true;
-
-for (int i = from; i <=to; i ++){
-    if (isdigit(str[i]) && integer_part){
-        num *=10.0;
-        num += (str[i] - '0');
-    } else if(str[i] == '.')
-        integer_part = false;
-    else if (isdigit(str[i]) && !integer_part){
-        decimal /=10.0;
-        num +=((str[i] - '0') * decimal);
-    
-    }else
-        break;
-}
-if(minus) num *=-1.0;
-return num;
-
-}
-Complex Complex::operator+(const Complex& c){
-   Complex temp(real + c.real, img + c.img);
-    return temp;
-}
-Complex Complex::operator-(const Complex& c){
-    Complex temp(real - c.real, img - c.img);
-    return temp;
-}
-
-Complex Complex::operator*(const Complex& c){
-    Complex temp(real * c.real - img * c.img, real * c.img + img *c.real);
-    return temp;
-}
-
-Complex Complex::operator/(const Complex& c){
-    Complex temp((real * c.real + img * c.img)/(c.real * c.real + c.img * c.img), (img*c.real - real * c.img) / (c.real * c.real + c.img*c.img));
-    return temp;
-}
-
-Complex& Complex::operator=(const Complex& c){
-    real = c.real;
-    img = c.img;
-    return *this;
-}
-
-Complex Complex::operator+(const char* str){
-    Complex temp(str);
-    return (*this) + temp;
-}
-
-Complex Complex::operator-(const char* str){
-    Complex temp(str);
-    return (*this) - temp;
-}
-Complex Complex::operator*(const char* str){
-    Complex temp(str);
-    return (*this) * temp;
-}
-
-Complex Complex::operator/(const char* str){
-    Complex temp(str);
-    return (*this) / temp;
-}
-
-
-   Complex& Complex::operator+=(const Complex& c){
-    (*this)=(*this) +c;
-    return *this;}
-           Complex& Complex::operator-=(const Complex& c){
-    (*this)=(*this) -c;
-    return *this;}
-           Complex& Complex::operator*=(const Complex& c){
-    (*this)=(*this) *c;
-    return *this;}
-           Complex& Complex::operator/=(const Complex& c){
-    (*this)=(*this)/c;
-    return *this;}
+char& MyString::operator[](const int index) {return string_content[index];}
 
 int main(){
-Complex a(0,0);
-a = "-1.1 = i3.923"+a;
-std::cout << "a의 값은 : " << a << "이다" << std::endl;
-}
+    MyString str("abcdef");
+    str[3] = 'c';
 
+    str.println();
+}

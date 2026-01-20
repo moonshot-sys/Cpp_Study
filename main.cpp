4a1d8d474ba0cdb4158366e6575e5af238a7659e
answj <moonshot@ajou.ac.kr>
2026-01-20 13:24:56 +0900

    2

--- main.cpp
+++ main.cpp
@@ -3,13 +3,18 @@
 
 //해결방안
 #include <iostream>
-#include <cstring>
-using namespace std;
-int main(){
-    int t;
-    while (std::cin >> t){
-        std::cout << "입력 :: " << t << std::endl;
-        if(t==0) break;
-    }
-}
+#include <string>
 
+int main(){
+  int t;
+  while (true){
+    std::cin >> t;
+    std::cout<<"입력 :: "<< t << std::endl;
+    if(std::cin.fail()){
+        std::cout<<"제대로 입력해주세요"<<std::endl;
+        std::cin.clear();          //플래그들을 초기화 하고
+        std::cin.ignore(100, '\n');//개행문자가 나올 때 까지 무시한다.
+    }
+    if(t==0) break;
+  }
+}

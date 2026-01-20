b11e04204139f68e3d0c47f64c95195a821f8f99
answj <moonshot@ajou.ac.kr>
2026-01-20 15:04:39 +0900

    getline으로 읽어들이기

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
  //이 파일은 연습용이다.
 // 틀려도 되고, 정리 안 해도 된다.
 
-//파일 전체 읽기
+//getline 으로 읽어들이기
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -9,27 +9,22 @@
 int main(){
     //파일 읽기 준비
     std::ifstream in("test.txt.txt");
-    std::string s;
-if(in.is_open()){
-    //위치 지정자를 파일 끝으로 옮긴다.
-    in.seekg(0, std::ios::end);
+    char buf[100];
 
-    //그리고 그 위치를 읽는다.(파일의 크기)
-    int size = in.tellg();
+    if(!in.is_open()){
+        std::cout<<"파일을 찾을 수 없습니다!"<<std::endl;
+        return 0;
+    }
 
-    //그 크기의 문자열을 할당한다.
-    s.resize(size);
-
-    //위치 지정자를 다시 파일 맨 앞으로 옮긴다.
-    in.seekg(0, std::ios::beg);
-
-    //파일 전체 내용을 읽어서 문자열에 저장한다.
-    in.read(&s[0], size);
-    std::cout<<s<<std::endl;
-
-}else{
-    std::cout<<"파일을 찾을 수 없습니다!"<<std::endl;
-}
-
-return 0;
+    while(in){
+        in.getline(buf, 100, '.'); //getline 함수는 파일에서 개행문자 (\n)이 나올때까지
+                              // 최대 지정한 크기 -1 만큼 읽게 된다. 
+                              //이유는 buf의 맨 마지막 문자로 널 종료 문자를 넣어줘야 하기 떄문에
+                              //이 경우 buf에 최대 99 글자 까지 입력을 받는다.
+                              //이 밖에도 내가 지정한 문자까지 읽는 것으로 바꿀 수 있다.
+                              //in.getline(buf, 100, '.');
+                              //마침표가 나올 때 까지 입력 받음.
+        std::cout << buf <<std::endl;
+    }
+    return 0;
 }

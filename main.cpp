9545326e234c8e1f3e939d7c07ddf9c1901d4eda
answj <moonshot@ajou.ac.kr>
2026-01-20 13:15:15 +0900

    istream 클래스 1

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
  //이 파일은 연습용이다.
 // 틀려도 되고, 정리 안 해도 된다.
 
+//해결방안
 #include <iostream>
 #include <cstring>
 using namespace std;
 int main(){
     int t;
-    while (true){
-        std::cin >>t;
-        std::cout << "입력 :: "<< t <<std::endl;
+    while (std::cin >> t){
+        std::cout << "입력 :: " << t << std::endl;
         if(t==0) break;
     }
 }
+

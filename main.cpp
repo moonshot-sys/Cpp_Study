5337cd4c34677e5bab5a058ec2a93bcdaa918f89
answj <moonshot@ajou.ac.kr>
2026-01-20 14:49:17 +0900

    파일을 2진수로 읽기

--- main.cpp
+++ main.cpp
@@ -1,19 +1,22 @@
  //이 파일은 연습용이다.
 // 틀려도 되고, 정리 안 해도 된다.
 
-//C++파일 입출력
+//이진수로 읽기
 #include <fstream>
 #include <iostream>
 #include <string>
-int main() {
-// 파일 읽기 준비
-std::ifstream in("test.txt.txt");
-std::string s;
-if (in.is_open()) {
-in >> s;
-std::cout << "입력 받은 문자열 :: " << s << std::endl;
-} else {
-std::cout << "파일을 찾을 수 없습니다!" << std::endl;
-}
-return 0;
+
+int main(){
+    //파일 읽기 준비
+    std::ifstream in("test.txt.txt", std::ios::binary);
+    std::string s;
+
+    int x;
+    if (in.is_open()){
+        in.read((char*)(&x),4);
+        std::cout<<std::hex<<x<<std::endl;
+    }else{
+        std::cout<<"파일을 찾을 수 없습니다." <<std::endl;
+    }
+    return 0;
 }

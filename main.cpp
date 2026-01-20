2d806974c7d33cb0c3e5f71743127d7f270cd87b
answj <moonshot@ajou.ac.kr>
2026-01-20 14:58:12 +0900

    파일 전체 읽기

--- main.cpp
+++ main.cpp
@@ -1,22 +1,35 @@
  //이 파일은 연습용이다.
 // 틀려도 되고, 정리 안 해도 된다.
 
-//이진수로 읽기
+//파일 전체 읽기
 #include <fstream>
 #include <iostream>
 #include <string>
 
 int main(){
     //파일 읽기 준비
-    std::ifstream in("test.txt.txt", std::ios::binary);
+    std::ifstream in("test.txt.txt");
     std::string s;
+if(in.is_open()){
+    //위치 지정자를 파일 끝으로 옮긴다.
+    in.seekg(0, std::ios::end);
 
-    int x;
-    if (in.is_open()){
-        in.read((char*)(&x),4);
-        std::cout<<std::hex<<x<<std::endl;
-    }else{
-        std::cout<<"파일을 찾을 수 없습니다." <<std::endl;
-    }
-    return 0;
+    //그리고 그 위치를 읽는다.(파일의 크기)
+    int size = in.tellg();
+
+    //그 크기의 문자열을 할당한다.
+    s.resize(size);
+
+    //위치 지정자를 다시 파일 맨 앞으로 옮긴다.
+    in.seekg(0, std::ios::beg);
+
+    //파일 전체 내용을 읽어서 문자열에 저장한다.
+    in.read(&s[0], size);
+    std::cout<<s<<std::endl;
+
+}else{
+    std::cout<<"파일을 찾을 수 없습니다!"<<std::endl;
+}
+
+return 0;
 }

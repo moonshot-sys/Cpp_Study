ee37c64c80471e032520c0aa222a6e16565c880a
answj <moonshot@ajou.ac.kr>
2026-01-21 09:12:23 +0900

    파일 전체를 한 줄 씩 읽기

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
  //이 파일은 연습용이다.
 // 틀려도 되고, 정리 안 해도 된다.
 
-//getline 으로 읽어들이기
+//std::string에 정의된 getline 사용
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -15,16 +15,14 @@ int main(){
         std::cout<<"파일을 찾을 수 없습니다!"<<std::endl;
         return 0;
     }
-
-    while(in){
-        in.getline(buf, 100, '.'); //getline 함수는 파일에서 개행문자 (\n)이 나올때까지
-                              // 최대 지정한 크기 -1 만큼 읽게 된다. 
-                              //이유는 buf의 맨 마지막 문자로 널 종료 문자를 넣어줘야 하기 떄문에
-                              //이 경우 buf에 최대 99 글자 까지 입력을 받는다.
-                              //이 밖에도 내가 지정한 문자까지 읽는 것으로 바꿀 수 있다.
-                              //in.getline(buf, 100, '.');
-                              //마침표가 나올 때 까지 입력 받음.
-        std::cout << buf <<std::endl;
+    std::string s;
+    while (std::getline(in,s)){//getline 함수는 ifstream에 정의되어 있는 것이 아니라
+                               //std::string에 정의되어 있는 함수로
+                               //첫 번째 인자로 istream 객체를 받고,
+                               //두 번째 인자로 입력 받은 문자열을 저장할 string 객체를 받는다.
+        std::cout<<s<<std::endl;
     }
     return 0;
-}
+}//주의사항 ** while 문 조건으로 절대 in.eof()를 사용하면 안된다.
+// eof 함수는 파일 위치 지시자가 파일에 끝에 도달한 이후에 true를 리턴하기 때문이다.
+//in.eof()는 while 문 안에서 파일 읽기가 안전하다 라는 것을 보장하지 않는다.

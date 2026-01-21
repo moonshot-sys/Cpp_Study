c7bcedfd54136fef93a6c9f6fbcdd0a6c6a9fa84
answj <moonshot@ajou.ac.kr>
2026-01-21 09:15:42 +0900

    파일 쓰기

--- main.cpp
+++ main.cpp
@@ -1,28 +1,21 @@
  //이 파일은 연습용이다.
 // 틀려도 되고, 정리 안 해도 된다.
 
-//std::string에 정의된 getline 사용
+//파일에 쓰기
 #include <fstream>
 #include <iostream>
 #include <string>
 
 int main(){
-    //파일 읽기 준비
-    std::ifstream in("test.txt.txt");
-    char buf[100];
+    //파일 쓰기 준비
+    std::ofstream out("test.txt.txt");
 
-    if(!in.is_open()){
-        std::cout<<"파일을 찾을 수 없습니다!"<<std::endl;
-        return 0;
-    }
     std::string s;
-    while (std::getline(in,s)){//getline 함수는 ifstream에 정의되어 있는 것이 아니라
-                               //std::string에 정의되어 있는 함수로
-                               //첫 번째 인자로 istream 객체를 받고,
-                               //두 번째 인자로 입력 받은 문자열을 저장할 string 객체를 받는다.
-        std::cout<<s<<std::endl;
+    if(out.is_open()){
+        out<< "이걸쓰자~~";
     }
     return 0;
-}//주의사항 ** while 문 조건으로 절대 in.eof()를 사용하면 안된다.
-// eof 함수는 파일 위치 지시자가 파일에 끝에 도달한 이후에 true를 리턴하기 때문이다.
-//in.eof()는 while 문 안에서 파일 읽기가 안전하다 라는 것을 보장하지 않는다.
+}
+//이 경우 파일이 존재한다면 파일의 내용을 다 지우고 새로운 내용으로 덮어 쓰게 된다.
+//파일이 존재 하지 않으면 파일을 새롭게 생성하고 성공했다면 출력한다.
+    

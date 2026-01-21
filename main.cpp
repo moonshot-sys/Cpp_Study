08c56c9d123ca93d24a43d24e69181156fcbc7a8
answj <moonshot@ajou.ac.kr>
2026-01-21 09:36:52 +0900

    std::ofstream 연산자 오버로딩

--- main.cpp
+++ main.cpp
@@ -1,28 +1,33 @@
  //이 파일은 연습용이다.
 // 틀려도 되고, 정리 안 해도 된다.
 
-//ate 와 app
+//std::ofstream 연산자 오버로딩 하기
 #include <fstream>
 #include <iostream>
 #include <string>
 
+class Human{
+    std::string name;
+    int age;
+
+    public:
+    Human(const std::string& name, int age) : name(name), age(age) {}
+    std::string get_info(){
+        return "Name :: " + name + "/ Age :: " + std::to_string(age);
+    }
+
+    friend std::ofstream& operator<<(std::ofstream& o, Human& h);
+};
+std::ofstream& operator<<(std::ofstream& o, Human& h){
+    o<<h.get_info();
+    return o;
+}
+
 int main(){
-    //두 파일에는 모두 abc라고 써 있었습니다. 
-    std::ofstream out("test.txt.txt",std::ios::app);
-    std::ofstream out2("other.txt",std::ios::out);
-
-    out.seekp(3, std::ios::beg);//파일 위치 지정자를 앞에서 3칸 떨어진 곳으로 이동
-    out2.seekp(3, std::ios::beg);
-
-    out<<"추가";
-    out2<<"추가";
+    //파일 쓰기 준비
+    std::ofstream out("test.txt.txt");
+    Human h("이재범",60);
+    out<< h<< std::endl;
 
     return 0;
-
-//app을 사용한 경우 abc 바로 뒤에 '추가' 문자열이 붙어 있는 것으로 나타남.
-//app 모드로 읽었을 때 현재 파일은 빈 파일이라 생각되어 위치 지정자라 움직일 곳이 없기에,
-//실제로 위치 지정자는 움직이지 않고 출력.
-
-//ate를 사용한 경우 3칸 띄어진 곳에서 '추가'라고 문자열이 출력된 반면 기존의 abc라는 데이터는 지워짐.
-//ate는 기존 파일의 내용을 보존하지 않음.
 }
